a8f0c801dc94a299ec1fde21d695606ac543087f
oussama <toumi10oussama@gmail.com>
2023-06-08 17:29:50 -0700

    éllé

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    sume = 0
+    for i in range(count)
+        sume = sume + int(sys.argv[i + 1])
+    print("{}".format(sume))
