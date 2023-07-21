965f45fdd24e1e224a8184647db5cb86ab033798
oussama toumi <toumi11oussama@gmail.com>
2023-07-21 22:56:07 +0100

    add raises

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,27 @@ class Rectangle(Base):
             str: A formatted string representing the Rectangle.
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+   
+    def update(self, *args):
+        """Update the rectangle
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if len(args) > 0:
+            self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+                if len(args) > 2:
+                    self.height = args[2]
+                    if len(args) > 3:
+                        self.x = args[3]
+                        if len(args) > 4:
+                            self.y = args[4]
+                        
