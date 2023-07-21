7ca779741eb8ea336065493dd8bc9ad643c9be5e
oussama toumi <toumi11oussama@gmail.com>
2023-07-21 23:47:32 +0100

    add raises

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -142,3 +142,13 @@ class Rectangle(Base):
                             self.x = args[3]
                             if len(args) > 4:
                                 self.y = args[4]               
+    
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+"""Defines a rectangle model class."""
+import json
+import csv
+import turtle
+from models.rectangle import Rectangle
+
+
+class Square (Rectangle):
+    """Rectangle square.
+
+    This Represents the Class square.
+
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
