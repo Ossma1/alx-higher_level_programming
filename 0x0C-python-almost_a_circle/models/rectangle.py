37ba191f0ff292bf47e35062329fc69094bda1f5
oussama toumi <toumi11oussama@gmail.com>
2023-07-21 20:38:58 +0100

    add raises

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -48,13 +48,12 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if type(value) == int:
-            if value >= 0:
-                self.__height = value
-            else:
-                raise ValueError("height must be > 0")
-        else:
+        if type(value) != int:
             raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
             
 
     @property
@@ -63,13 +62,11 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if type(value) == int:
-            if value >= 0:
-                self.__x = value
-            else:
-                raise ValueError("x must be > 0")
-        else:
+        if type(value) != int:
             raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
@@ -77,10 +74,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if type(value) == int:
-            if value >= 0:
-                self.__y = value
-            else:
-                raise ValueError("y must be > 0")
-        else:
+        if type(value) != int:
             raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
