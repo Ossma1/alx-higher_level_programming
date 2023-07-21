e7d21467b95e1abb2617e809ea5c5619f452f896
oussama toumi <toumi11oussama@gmail.com>
2023-07-21 20:35:13 +0100

    add raises

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -36,13 +36,11 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) == int:
-            if value >= 0:
-                self.__width = value
-            else:
-                raise ValueError("width must be > 0")
-        else:
+        if type(value) != int:
             raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
